42d094a1870c750f751e2814c2851e27fe2cb222
cyrusmp <cype1650@gmail.com>
2025-06-15 14:49:06 -0700

    Changed instantiation to take in DictionaryLoader object so that searches can be conducted on the loaded dictionary

--- Main.java
+++ Main.java
@@ -2,19 +2,19 @@ public class Main {
     public static void main(String[] args){
         System.out.println();
         System.out.println("! Loading Data...");
-        DictionaryLoader loader = new DictionaryLoader();
-        loader.loadDictionary();                            
+        DictionaryLoader loader = new DictionaryLoader();   //calls constructor in DictionaryLoader class to create new DictionaryLoader object that consists of an EnumMap data structure
+        loader.loadDictionary(); //call to loadDictionary function that loads enum constants into EnumMap                        
         System.out.println("! Loading completed...");
         System.out.println();
         System.out.println("===== DICTIONARY 340 JAVA =====");
 
-        int numKeywords = loader.countKeywords();
+        int numKeywords = loader.countKeywords();  //call to count number of keywords in EnumMap
         System.out.println("----- Keywords: " + numKeywords);
-        int numDefinitions = loader.countDefinitions();
+        int numDefinitions = loader.countDefinitions();  //call to count number of definitions in EnumMap
         System.out.println("----- Definitions: " + numDefinitions); 
         System.out.println("");
-        
-        InteractiveDictionary start = new InteractiveDictionary();
-        start.searchBar();
+
+        InteractiveDictionary start = new InteractiveDictionary(loader);  //call to instantiate InteractiveDictionary object with contents of DictionaryLoader object
+        start.searchBar();  //call to method implemented on InteractiveDictionary objects
     }
 }
