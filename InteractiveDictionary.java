006d3f94a6e6724d3dfa1d8f16a3f6b52d2e1f0e
cyrusmp <cype1650@gmail.com>
2025-06-15 15:20:05 -0700

    Reorganized !help and !q

--- InteractiveDictionary.java
+++ InteractiveDictionary.java
@@ -16,34 +16,51 @@ public class InteractiveDictionary {
             System.out.print("Search [" + searches + "]: ");
             String parameters = input.nextLine();
             String[] inputParams = parameters.split("\\s");
-             
-            for (String inputs : inputParams ) { 
-                if (inputs.equalsIgnoreCase("!q")) { 
-                    return;
-                } else if (inputs.equalsIgnoreCase("!help") || inputs.equals("")) {
-                    System.out.println("    |");
-                    System.out.println("     PARAMETER HOW-TO,  please enter:");
-                    System.out.println("     1. A search key -then 2. An optional part of speech -then");
-                    System.out.println("     3. An optional 'distinct' -then 4. An optional 'reverse'");
-                }
-            }
 
             String param1 = inputParams.length > 0 ? inputParams[0] : "";
             String param2 = inputParams.length > 1 ? inputParams[1] : "";
             String param3 = inputParams.length > 2 ? inputParams[2] : "";
             String param4 = inputParams.length > 3 ? inputParams[3] : "";
 
-            boolean putTopLine = false;
+            if (param1.equals("!help") || param1.equals("")) {
+                System.out.println("    |");
+                System.out.println("     PARAMETER HOW-TO,  please enter:");
+                System.out.println("     1. A search key -then 2. An optional part of speech -then");
+                System.out.println("     3. An optional 'distinct' -then 4. An optional 'reverse'");
+                System.out.println("    |");
+                searches++;
+                continue;
+            }
 
+            if (param1.equalsIgnoreCase("!q")) {
+                System.out.println();
+                System.out.println("-----THANK YOU-----");
+                System.out.println();
+                return;
+            }
+
+            boolean putTopLine = false;
+            boolean match = false;
+            
             for (EnumMap.Entry<DictionaryTerms, String[]> entry : loader.getDictionary().entrySet()) {
                 String keyword = entry.getValue()[0];
                 String partOfSpeech = entry.getValue()[1];
                 String definition = entry.getValue()[2];
 
                 if (keyword.equalsIgnoreCase(param1)) {
-
+                    match = true;
+                    if (!putTopLine) {
+                        System.out.println("    |");
+                        putTopLine = true;
+                    }                
                 } else if (!keyword.equalsIgnoreCase(param1)) {
-
+                    System.out.println("    |");
+                    System.out.println(" <NOT FOUND> To be considered for the next release. Thank you.");
+                    System.out.println("    |");
+                    System.out.println("    |");
+                    System.out.println("     PARAMETER HOW-TO,  please enter:");
+                    System.out.println("     1. A search key -then 2. An optional part of speech -then");
+                    System.out.println("     3. An optional 'distinct' -then 4. An optional 'reverse'");
                 }
             }
 
