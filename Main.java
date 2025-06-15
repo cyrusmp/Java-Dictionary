417a62c37eff68522519ed9efe65c92e593cf949
cyrusmp <cype1650@gmail.com>
2025-06-15 02:14:48 -0700

    Fixed format

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args){
+        System.out.println();
         System.out.println("! Loading Data...");
         DictionaryLoader loader = new DictionaryLoader();
-        loader.loadDictionary();                            //calls loadDictionary to input data into loader
+        loader.loadDictionary();                            
         System.out.println("! Loading completed...");
         System.out.println();
         System.out.println("===== DICTIONARY 340 JAVA =====");
