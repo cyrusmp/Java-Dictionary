9d921f37caa570343c59a38c94c8608889faaddc
cyrusmp <cype1650@gmail.com>
2025-06-15 02:27:10 -0700

    Fixed format

--- InteractiveDictionary.java
+++ InteractiveDictionary.java
@@ -26,12 +26,16 @@ public class InteractiveDictionary {    //declare publi class
             String param3 = inputParams.length > 2 ? inputParams[2] : "";
             String param4 = inputParams.length > 3 ? inputParams[3] : "";
 
-            System.out.println("    |");
+            boolean putTopLine = false;
 
             for (DictionaryTerms term : DictionaryTerms.values()) {
                 if (term.getKeyword().equalsIgnoreCase(param1)) {
                     if (param2.isEmpty() || term.getPartOfSpeech().equalsIgnoreCase(param2) && param3.isEmpty()) {
-                        System.out.println("     " + term.getKeyword() + " " + term.getPartOfSpeech() + " : " + term.getDefinition());
+                        while(!putTopLine) {
+                            System.out.println("    |");
+                            putTopLine = true;
+                        }
+                        System.out.println("     " + "[" + term.getKeyword() + "]" + " " + term.getPartOfSpeech() + " : " + term.getDefinition());
                     } else if (param2.equalsIgnoreCase(term.getPartOfSpeech()) && param3.equalsIgnoreCase("distinct")) {
                         System.out.println("     " + "[" + term.getKeyword() + "]" + " " + term.getPartOfSpeech() + " : " + term.getDefinition());
                     }
