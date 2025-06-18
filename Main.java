56e11bf2f6b2ea106356d64a59c6c29344956755
cyrusmp <cype1650@gmail.com>
2025-06-17 17:49:52 -0700

    Comments

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args){
-        DictionaryLoader loader = new DictionaryLoader();   //calls constructor in DictionaryLoader class to create new DictionaryLoader object that consists of an EnumMap data structure
-        loader.loadDictionary(); //call to loadDictionary function that loads enum constants into EnumMap                        
-        loader.countKeywords();
-        loader.countDefinitions(); 
+        
+        //calls constructor in DictionaryLoader class to create new DictionaryLoader object that consists of an EnumMap data structure
+        DictionaryLoader dictionary = new DictionaryLoader();
 
-        InteractiveDictionary start = new InteractiveDictionary(loader);  //call to instantiate InteractiveDictionary object with contents of DictionaryLoader object
-        start.searchBar();  //call to search InteractiveDictionary with loaded dictionary data
+        //call to loadDictionary method
+        dictionary.loadDictionary();
+        
+        //call to countKeywords method
+        dictionary.countKeywords();
+
+        //call to countDefinictions method 
+        dictionary.countDefinitions(); 
+
+        //calls constructor in InteractiveDictionary to initialize new object with contents from DictionaryLoader
+        InteractiveDictionary start = new InteractiveDictionary(dictionary);  
+
+        //call to searchBar() method that interacts with dictionary conents
+        start.searchBar(); 
     }
 }
