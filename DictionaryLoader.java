5bd443b087bf2f776b618247972ce91fec715e75
cyrusmp <cype1650@gmail.com>
2025-06-17 17:47:59 -0700

    Comments

--- DictionaryLoader.java
+++ DictionaryLoader.java
@@ -7,45 +7,68 @@ import java.util.ArrayList;
 
 public class DictionaryLoader {
 
+    //declare EnumMap data structure titled 'dictionaryMap' -> 
+    //Key: DictionaryTerms, Value: 2D array of Strings: [0] keyword, [1] parts of speech, [2] definitions 
     private EnumMap<DictionaryTerms, String[][]> dictionaryMap;
 
+    //constructer to initialize a new EnumMap for storing dictionary entries from DictionaryTerms
     public DictionaryLoader() {                                
         dictionaryMap = new EnumMap<>(DictionaryTerms.class);
     }
 
+    //loads dictionary data from DictionaryTerms enum into the EnumMap
     public void loadDictionary() {
         System.out.println();
         System.out.println("! Loading Data...");
-        for (DictionaryTerms term : DictionaryTerms.values()) {
+
+        //iterate through each entry in DictionaryTerms, extract and store its data
+        for (DictionaryTerms entry : DictionaryTerms.values()) {
+
+            //2D array with 3 rows to hold data
             String[][] data = new String[3][];
-            data[0] = new String[] {term.getKeyword()};
-            data[1] = term.getPartOfSpeech();
-            data[2] = term.getDefinition();
-            dictionaryMap.put(term, data);
+            data[0] = new String[] {entry.getKeyword()};    //row 0
+            data[1] = entry.getPartOfSpeech();              //row 1
+            data[2] = entry.getDefinition();                //row 2
+            
+            //store data in the EnumMap
+            dictionaryMap.put(entry, data);
         }
         System.out.println("! Loading completed...");
         System.out.println();
         System.out.println("===== DICTIONARY 340 JAVA =====");
     }
 
+    //retrieve the dictionary of type EnumMap with DictionaryTerms as key and 2D String array as value
     public EnumMap<DictionaryTerms, String[][]> getDictionary() {
         return dictionaryMap;
     }
 
+    //counts number of unique keywords
     public void countKeywords() {
+
+        //A set of strings representing keywords is equal to a new HashSet implementation
         Set<String> keywords = new HashSet<>();
-        for (DictionaryTerms term : dictionaryMap.keySet()) {  
-            keywords.add(term.getKeyword());
+
+        //iterate through every entry of type DictionaryTerms, retrieve its data
+        //add keyword from values to set 
+        for (DictionaryTerms entry : dictionaryMap.keySet()) {  
+            keywords.add(entry.getKeyword());
         }
         int keywordSize = keywords.size();
         System.out.println("----- Keywords: " + keywordSize);
     }
 
+    //counts definitions
     public void countDefinitions() {
+
+        //A list of strings is equal to an ArrayList implementation (can hold duplicate definitions)
         List<String> definitions = new ArrayList<>();
-        for (DictionaryTerms term : DictionaryTerms.values()) {
-            for (String def : term.getDefinition()) {
-                definitions.add(def);
+
+        //iterate through each entry of type DictionaryTerms, retrieve its data
+        //add definitions to list
+        for (DictionaryTerms entry : DictionaryTerms.values()) {
+            for (String definition : entry.getDefinition()) {
+                definitions.add(definition);
             }
         }
         int definitionSize = definitions.size();
